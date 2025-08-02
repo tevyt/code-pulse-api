87d9a8b2b6b00fc37476366c4a75b803ce1dcc55
Travis Smith <travis.a.smith.ja@gmail.com>
2025-08-02 18:05:37 -0400

    Initial commit.

---
+++ src/main/java/com/tevyt/code_pulse_api/CodePulseApiApplication.java
@@ -0,0 +1,13 @@
+package com.tevyt.code_pulse_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePulseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodePulseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tevyt/code_pulse_api/CodePulseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tevyt.code_pulse_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePulseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
